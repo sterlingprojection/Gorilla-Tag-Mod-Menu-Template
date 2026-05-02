ec6e11955fb6dafcd0cbcd8784320f2d84a1a235
sterling <73369601+sterlingprojection@users.noreply.github.com>
2026-05-01 23:39:48 -1200

    Update Example.cs

--- Features/Example.cs
+++ Features/Example.cs
@@ -1,1 +1,37 @@
+using System.Linq;
+using Photon.Pun;
+using Testplate.Menu;
+using Testplate.Utils;
+using UnityEngine;
 
+namespace Testplate.Features
+{
+    public class ExampleGun : FeatureBase
+    {
+        public override string Name => "Example Gun";
+
+        public override void OnUpdate()
+        {
+            RaycastHit hit;
+            GunLib.UpdateGun(out hit);
+
+            if (!GunLib.IsShooting())
+                return;
+
+            VRRig targetRig = GunLib.GetLockedRig();
+
+            if (targetRig == null && hit.collider != null)
+                targetRig = hit.collider.GetComponentInParent<VRRig>();
+
+            if (targetRig == null || targetRig == GorillaTagger.Instance.offlineVRRig)
+                return;
+
+            var netPlayer = targetRig.GetPlayer();
+
+            if (netPlayer != null)
+            {
+                Debug.Log($"Example target selected: {netPlayer.NickName}");
+            }
+        }
+    }
+}
